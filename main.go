61dd6110c8f2d94c3abc3d58911cc758398c1873
mokhs00 <mokhs00@naver.com>
2022-03-27 16:52:48 +0900

    add : users proto listener
    - GetUser
    - ListUser

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"context"
+	"go-grpc-playground/data"
+	userpb "go-grpc-playground/protos/v1/users"
 	"google.golang.org/grpc"
 	"log"
 	"net"
 )
 
 const port = "8000"
+
+type userServer struct {
+	userpb.UserServer
+}
 
 func main() {
 	listen, err := net.Listen("tcp", ":"+port)
@@ -16,11 +23,39 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
+	userpb.RegisterUserServer(grpcServer, &userServer{})
 
 	log.Printf("start gRPC server on %s port", port)
 
 	if err := grpcServer.Serve(listen); err != nil {
 		log.Fatalf("failed to serve gRPC server: %s", err)
 	}
+
+}
+
+func (s *userServer) GetUser(ctx context.Context, req *userpb.GetUserRequest) (*userpb.GetUserResponse, error) {
+	userId := req.UserId
+
+	var userInfo *userpb.UserInfo
+
+	for _, user := range data.Users {
+		if user.UserId == userId {
+			userInfo = user
+			break
+		}
+	}
+
+	return &userpb.GetUserResponse{User: userInfo}, nil
+}
+
+func (s *userServer) ListUser(ctx context.Context, req *userpb.ListUserRequest) (*userpb.ListUserResponse, error) {
+	userInfoList := make([]*userpb.UserInfo, len(data.Users))
+	for i, user := range data.Users {
+		userInfoList[i] = user
+	}
+
+	return &userpb.ListUserResponse{
+		Users: userInfoList,
+	}, nil
 
 }
